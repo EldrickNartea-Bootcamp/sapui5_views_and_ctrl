6eed34db732dee4b4bc08800bb104093cd00113c
Eldrick Nartea <eldrick.nartea@accenture.com>
2025-05-22 10:31:23 +0000

    Initial Commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zbtp/sapui5viewsandctrl/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zbtp/sapui5viewsandctrl/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
